f54194cc2e39c8dac8836cba61acda2a1389a651
balasuar <balasuar@gmail.com>
2017-01-17 06:13:45 +0530

    Adding WIP Route

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var express = require('express');
+var request = require('supertest');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send(new Date());
 });
 
+app.get('/convert/:currency/:value', function (req, res) {
+    //hint switch on req.params.currency
+});
+
 app.listen(3000, function () {
   console.log('Test app listening on port 3000!')
-})
+});
+
+
+
+//tests
+
+request(app)
+  .get('/')
+  .expect('Content-Type', /json/)
+  .expect(200)
+  .end(function(err, res) {
+    if (err) throw err;
+});
